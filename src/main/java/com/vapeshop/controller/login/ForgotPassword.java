ad9ca4b3fbf05f583b2e80b86146e4e4cb1c6b55
NgocNhan43 <nguyenngocnhan442002@gmail.com>
2023-10-09 15:52:29 +0700

    test forgot

--- src/main/java/com/vapeshop/controller/login/ForgotPassword.java
+++ src/main/java/com/vapeshop/controller/login/ForgotPassword.java
@@ -5,17 +5,22 @@
 package com.vapeshop.controller.login;
 
 
-
-import com.vapeshop.respository.SendMail;
-
+import com.vapeshop.entity.User;
+import com.vapeshop.respository.UserRespository;
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import java.io.IOException;
-
+import java.util.Properties;
+import java.util.Random;
 
 @WebServlet("/forgotPassword")
 public class ForgotPassword extends HttpServlet {
@@ -23,8 +28,62 @@ public class ForgotPassword extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         String email = request.getParameter("email");
-        SendMail.forgotPass(email,"123");
-        response.sendRedirect("index.jsp");
+        RequestDispatcher dispatcher = null;
+        int otpvalue = 0;
+        HttpSession mySession = request.getSession();
+
+        if (email != null || !email.equals("")) {
+
+            UserRespository dao = new UserRespository();
+            User check = dao.CheckEmailtExist(email);
+            if (check == null) {
+                // account không tồn tại, load lại trang repassWord
+                request.getRequestDispatcher("forgotPassword.jsp").forward(request, response);
+
+                request.getSession().setAttribute("emailExist", "Your email is not exist ");
+
+            } else {
+                // sending otp
+                Random rand = new Random();
+                otpvalue = rand.nextInt(1255650);
+
+                String to = email;// change accordingly
+                // Get the session object
+                Properties props = new Properties();
+                props.put("mail.smtp.host", "smtp.gmail.com");
+                props.put("mail.smtp.socketFactory.port", "465");
+                props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+                props.put("mail.smtp.auth", "true");
+                props.put("mail.smtp.port", "465");
+                Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication("trinhquang96vu@gmail.com", "ojczliwiidcoeeje");// Put your email
+                        // id and
+                        // password here
+                    }
+                });
+                // compose message
+                try {
+                    MimeMessage message = new MimeMessage(session);
+                    message.setFrom(new InternetAddress(email));// change accordingly
+                    message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+                    message.setSubject("Hello");
+                    message.setText("your OTP is: " + otpvalue);
+                    // send message
+                    Transport.send(message);
+                    System.out.println("message sent successfully");
+                } catch (MessagingException e) {
+                    throw new RuntimeException(e);
+                }
+                dispatcher = request.getRequestDispatcher("EnterOtp.jsp");
+                request.setAttribute("message", "OTP is sent to your email id");
+                //request.setAttribute("connection", con);
+                mySession.setAttribute("otp", otpvalue);
+                mySession.setAttribute("email", email);
+                dispatcher.forward(request, response);
+                //request.setAttribute("status", "success");
+            }
+
 //        } else {
 //            // Email parameter is not provied or is empty
 //            request.getRequestDispatcher("forgotPassword.jsp").forward(request, response);
@@ -33,3 +92,4 @@ public class ForgotPassword extends HttpServlet {
 
     }
 
+}
