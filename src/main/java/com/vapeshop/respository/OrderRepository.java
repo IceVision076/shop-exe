9d65857040d91dfab9d10e99f185898c0c701df2
quang <quanggaming2002@gmail.com>
2023-11-02 09:59:33 +0700

    giỏ hàng 1.4.1 (pre update chi tiet don hang da dat)

--- src/main/java/com/vapeshop/respository/OrderRepository.java
+++ src/main/java/com/vapeshop/respository/OrderRepository.java
@@ -1,6 +1,7 @@
 package com.vapeshop.respository;
 
 import com.vapeshop.config.DBConnect;
+import com.vapeshop.entity.Items;
 import com.vapeshop.entity.Order;
 import com.vapeshop.entity.User;
 import com.vapeshop.respository.generator.Isvalid;
@@ -17,7 +18,8 @@ public class OrderRepository {
     public static String getOrderId() {
         try {
             String OrderId = RandomGenerator.generateRandomString();
-            String query = "select BillID from tblBill";
+            System.out.println("==>Order id random 1 : " +OrderId );
+            String query = "select order_id from [Order]";
             Connection con = DBConnect.getConnection();
             PreparedStatement stmt = con.prepareStatement(query);
             ResultSet results = stmt.executeQuery();
@@ -33,7 +35,7 @@ public class OrderRepository {
             }
             return OrderId;
         } catch (Exception e) {
-            System.out.println("Loi method checkExistOrder(Cart cart ) trong OrderRepository.java ");
+            System.out.println("Loi method getOrderId()trong OrderRepository.java ");
         }
         return null;
     }
@@ -71,10 +73,10 @@ public class OrderRepository {
             stmt.setObject(3, LocalDateTime.now());
             stmt.setString(4, null);
             if (cart.getPaymentType() == 0) {
-                stmt.setString(5, "Đang xử lý-COD");
+                stmt.setObject(5, "0");
 
             } else {
-                stmt.setString(5, "Đang xử lý-CK");
+                stmt.setObject(5, "1");
             }
             stmt.setString(6, cart.getDiscountCode());
             stmt.setString(7, user.getAddress());
@@ -99,18 +101,16 @@ public class OrderRepository {
     public static boolean createOrderDetail(Order cart, String orderId) {
         System.out.println("=>>>>....>>>>>>>>>>>>>>>>>>>>>>>" + orderId);
         System.out.println(cart.getCart());
-        System.out.println(cart.getCart().get(0).getProduct().getProductId());
-        System.out.println(cart.getCart().get(0).getProduct().getProductAmount());
-        System.out.println(cart.getCart().get(0).getProduct().getProductPrice());
+
         for (Items i : cart.getCart()) {
             try {
                 Connection con = DBConnect.getConnection();
-                String query = "insert into tblOrderDetails values (?,?,?,?)";
+                String query = "insert into OrderDetail values (?,?,?,?)";
                 PreparedStatement stmt = con.prepareStatement(query);
-                stmt.setString(1, orderId);
-                stmt.setString(2, i.getProduct().getProductId());
-                stmt.setInt(3, i.getAmmout());
-                stmt.setDouble(4, i.getPrice());
+                stmt.setString(1, orderId); //id cua cai bill
+                stmt.setString(2, i.getProductType().getProductTypeId()); //id cua san pham
+                stmt.setInt(3, i.getAmmout()); //so luong
+                stmt.setDouble(4, i.getPrice()); //gia luc mua
                 stmt.executeUpdate();
                 con.close();
             } catch (Exception e) {
