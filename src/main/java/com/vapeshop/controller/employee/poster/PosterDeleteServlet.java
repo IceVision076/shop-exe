b0c445197380afe0532133f1d2f1771ae8f07c0f
kitare17 <hoctapak@gmail.com>
2023-11-03 14:17:32 +0700

    .................

--- src/main/java/com/vapeshop/controller/employee/poster/PosterDeleteServlet.java
+++ src/main/java/com/vapeshop/controller/employee/poster/PosterDeleteServlet.java
@@ -1,10 +1,13 @@
 package com.vapeshop.controller.employee.poster;
 
+import com.vapeshop.entity.Poster;
 import com.vapeshop.respository.employee.PosterRespository;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
+
+import java.io.File;
 import java.io.IOException;
 
 @WebServlet(name = "PosterDeleteServlet", value = "/poster-delete")
@@ -18,8 +21,22 @@ public class PosterDeleteServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String postId = request.getParameter("postId");
         String page= request.getParameter("page");
+        Poster poster= PosterRespository.getPosterById(postId);
+        try {
+            File f=new File(".");
+            System.out.println("=>>>>>>>>>>"+f.getPath());
+            File file=new File(poster.getImgUrl());
+
+            if(file.exists()){
+                file.delete();
+                System.out.println("==========Xoa file "+poster.getImgUrl()+"==========");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
         PosterRespository.deletePoster(postId);
-        System.out.println("==================="+postId);
+        System.out.println("==================="+postId+"================");
         response.sendRedirect("poster-management?page="+page);
     }
 }
--- src/main/java/com/vapeshop/respository/employee/PosterRespository.java
+++ src/main/java/com/vapeshop/respository/employee/PosterRespository.java
@@ -27,15 +27,15 @@ public class PosterRespository {
             ResultSet rs = preparedStatement.executeQuery();
             list = new ArrayList<>();
             while (rs.next()) {
-                 String id=rs.getString("id");
-                 String imgUrl=rs.getString("url_image");
-                 String userId=rs.getString("user_id");
-                 LocalDateTime dateCreate=rs.getObject("date_create",LocalDateTime.class);
-                 char status=rs.getString("status").charAt(0);
-                User user=new User();
+                String id = rs.getString("id");
+                String imgUrl = rs.getString("url_image");
+                String userId = rs.getString("user_id");
+                LocalDateTime dateCreate = rs.getObject("date_create", LocalDateTime.class);
+                char status = rs.getString("status").charAt(0);
+                User user = new User();
                 user.setId(userId);
-                 Poster poster=new Poster( id,  imgUrl,  user,  dateCreate,status);
-                 list.add(poster);
+                Poster poster = new Poster(id, imgUrl, user, dateCreate, status);
+                list.add(poster);
             }
             connection.close();
         } catch (Exception e) {
@@ -43,6 +43,7 @@ public class PosterRespository {
         }
         return list;
     }
+
     public static int getPosterAmount() {
         int amount = 0;
         try {
@@ -60,14 +61,14 @@ public class PosterRespository {
         return amount;
     }
 
-    public static void releasePoster(String id){
+    public static void releasePoster(String id) {
         try {
             String query = "UPDATE Poster\n" +
                     "SET status=1\n" +
                     "WHERE id=?";
             Connection connection = DBConnect.getConnection();
             PreparedStatement preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setString(1,id);
+            preparedStatement.setString(1, id);
             preparedStatement.executeUpdate();
             connection.close();
         } catch (Exception e) {
@@ -75,14 +76,16 @@ public class PosterRespository {
         }
 
     }
-    public static void disablePoster(String id){
+
+
+    public static void disablePoster(String id) {
         try {
             String query = "UPDATE Poster\n" +
                     "SET status=0\n" +
                     "WHERE id=?";
             Connection connection = DBConnect.getConnection();
             PreparedStatement preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setString(1,id);
+            preparedStatement.setString(1, id);
             preparedStatement.executeUpdate();
             connection.close();
         } catch (Exception e) {
@@ -90,18 +93,19 @@ public class PosterRespository {
         }
 
     }
-    public static void addNewPoster(Poster poster){
+
+    public static void addNewPoster(Poster poster) {
         try {
             poster.setId(getNewPosterId());
             String query = "insert into Poster(id, url_image, user_id, status, date_create) \n" +
                     "values (?,?,?,?,?)\n";
             Connection connection = DBConnect.getConnection();
             PreparedStatement preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setString(1,poster.getId());
-            preparedStatement.setString(2,poster.getImgUrl());
-            preparedStatement.setString(3,poster.getUser().getId());
-            preparedStatement.setString(4,poster.getStatus()+"");
-            preparedStatement.setObject(5,poster.getDateCreate());
+            preparedStatement.setString(1, poster.getId());
+            preparedStatement.setString(2, poster.getImgUrl());
+            preparedStatement.setString(3, poster.getUser().getId());
+            preparedStatement.setString(4, poster.getStatus() + "");
+            preparedStatement.setObject(5, poster.getDateCreate());
             preparedStatement.executeUpdate();
             connection.close();
         } catch (Exception e) {
@@ -109,6 +113,7 @@ public class PosterRespository {
         }
 
     }
+
     public static String getNewPosterId() {
         String newID = "";
         try {
@@ -127,10 +132,9 @@ public class PosterRespository {
                     topID = topID + 0;
                 }
                 topID = topID + maxID;
-                newID="PT"+topID;
-            }
-            else {
-                newID="PT00000001";
+                newID = "PT" + topID;
+            } else {
+                newID = "PT00000001";
             }
 
 
@@ -140,12 +144,13 @@ public class PosterRespository {
         }
         return newID;
     }
-    public static void deletePoster(String id){
+
+    public static void deletePoster(String id) {
         try {
             String query = "delete Poster where id=?";
             Connection connection = DBConnect.getConnection();
             PreparedStatement preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setString(1,id);
+            preparedStatement.setString(1, id);
             preparedStatement.executeUpdate();
             connection.close();
         } catch (Exception e) {
@@ -153,8 +158,37 @@ public class PosterRespository {
         }
 
     }
+
+    public static Poster getPosterById(String posterId) {
+        Poster poster = null;
+        try {
+            String query = "select * from Poster\n" +
+                    "where id=?";
+            Connection connection = DBConnect.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, posterId);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            poster=new Poster();
+            if (resultSet.next()) {
+                 String id=resultSet.getString("id");
+                 String imgUrl=resultSet.getString("url_image");
+
+                 LocalDateTime dateCreate=resultSet.getObject("date_create",LocalDateTime.class);
+                 char status=resultSet.getString("status").charAt(0);
+            poster.setId(id);
+            poster.setImgUrl(imgUrl);
+            poster.setDateCreate(dateCreate);
+            poster.setStatus(status);
+            }
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return poster;
+    }
+
     public static void main(String[] args) {
-       getPosterPage(1).stream().forEach(System.out::println);
+        getPosterPage(1).stream().forEach(System.out::println);
     }
 
 }
